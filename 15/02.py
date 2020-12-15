4ddc5b250fd9c14af02bbe9c95960f0017c71639
MagicHatJo <42.jochang@gmail.com>
2020-12-15 12:16:56 -0800

    15

---
+++ 15/02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def memory_game(start, target):
+	spoken = {k: v for v, k in enumerate(start)}
+	current = start[-1]
+	for i in range(len(start) - 1, target - 1):
+		spoken[current], current = i, i - spoken[current] if current in spoken else 0
+	return current
+
+if __name__ == "__main__":
+	data = [19,20,14,0,9,1]
+	print(memory_game(data, 30000000))
